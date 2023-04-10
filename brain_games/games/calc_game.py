1fa26497dc41f6b0d07b2133e35e4b09890ce523
Andrey_Yudakov <viewsoul91@gmail.com>
2023-04-10 15:23:13 +0300

    Fix linter #step9

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -13,7 +13,7 @@ def calc_game():
     life_counter = 3  # счетчик попыток
     for _ in range(life_counter):
         math_signs = ["+", "-", "*"]
-        sign_in_game = choice(math_signs)  # генерируем случайный мат.знак
+        sign_in_game = choice(math_signs)  # генерируем случайный мат. знак
         x = create_random_int()  # генерируем случайное число
         if sign_in_game == "*":  # ограничение множителя,
             # чтобы можно было считать в уме
@@ -24,12 +24,13 @@ def calc_game():
         print(f"Question: {x} {sign_in_game} {y}")
         user_answer = int(input("Your answer: "))  # получаем ответ пользователя
 
-        if sign_in_game == "+":  # получаем корректный ответ
-            correct_answer = x + y
-        elif sign_in_game == "-":
-            correct_answer = x - y
-        else:
-            correct_answer = x * y
+        match sign_in_game:  # получаем правильный ответ
+            case "+":
+                correct_answer = x + y
+            case "-":
+                correct_answer = x - y
+            case _:
+                correct_answer = x * y
 
         result = give_result(user_answer, correct_answer, name)  # сравниваем
         # ответы и печатаем соответствующий текст
